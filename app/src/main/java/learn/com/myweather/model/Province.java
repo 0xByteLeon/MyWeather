4848f6a75094c08ab1a97706679135c0c04d5b5d
MoonlightSW <86625957@qq.com>
2016-01-05 20:15:15 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/learn/com/myweather/model/Province.java
@@ -0,0 +1,34 @@
+package learn.com.myweather.model;
+
+/**
+ * Created by MoonlightSW on 2016/1/4.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
